a63703ba3c1fc5d1f9193c954356b27ffbd061e2
Cymen Vig <cymenvig@gmail.com>
2013-10-11 01:39:12 -0500

    Get executable workign so you can run it with command line arguments for the URLs to check

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 
-//var urlOk = require('./lib/url-ok.js');
-//var pending = urlOk(["http://www.google.com/", "https://www.paypal.com/home", "http://localhost:4001/"], 40);
-var pending = require('./lib/status.js')("http://www.google.com/");
-pending.done(function(exitCode) {
-  process.exit(exitCode);
+var accumulator = require('./lib/accumulator');
+    failure = 1,
+    pending = accumulator(process.argv.slice(2))
+    success = 0;
+
+pending.done(function() {
+  process.exit(success);
 }, function() {
-  process.exit(1);
+  process.exit(failure);
 });
