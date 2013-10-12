029ab951c8ee5cb3e7973dd8f7d6278777e0cc47
Cymen Vig <cymenvig@gmail.com>
2013-10-12 01:20:04 -0500

    Add test for failure case to accumulator

--- spec/accumulator_spec.js
+++ spec/accumulator_spec.js
@@ -1,31 +1,49 @@
 'use strict';
 
 var accumulator = require('../lib/accumulator.js'),
-    nock = require('nock');
+    nock = require('nock'),
+    successCode = 200;
 
 describe('accumulator', function() {
     afterEach(function() {
         nock.cleanAll();
     });
 
-    it('resolves successfully when all urls are successful', function(done) {
-        var url = "http://localhost:32768/",
-            statusCode = 200,
+    it('resolves successfully when all URLs are successful', function(done) {
+        var urls = ['http://localhost:32001/', 'http://localhost:32002'],
             thirty_seconds_in_ms = 30 * 1000;
 
-        nock(url)
-            .head('/')
-            .reply(statusCode);
+        urls.forEach(function(url) {
+            nock(url)
+                .head('/')
+                .reply(successCode);
+        });
 
-        var url_b = "http://localhost:32769/";
-        nock(url_b)
-            .head('/')
-            .reply(statusCode);
-
-        var pending = accumulator([url, url_b], thirty_seconds_in_ms);
+        var pending = accumulator(urls, thirty_seconds_in_ms);
 
         pending.done(function() {
             done();
         });
+    });
+
+    it('errors out if one of the URLs is not successful', function(done) {
+        var urls = ['http://localhost:32001/', 'http://localhost:32002'],
+            one_second_in_ms = 1 * 1000;
+
+        urls.forEach(function(url) {
+            nock(url)
+                .head('/')
+                .reply(successCode);
+        });
+
+        urls.push('http://not.going.to.respond.localhost:32003/');
+
+        var pending = accumulator(urls, one_second_in_ms);
+
+        pending.done(
+            function() {},
+            function() {
+                done();
+            });
     });
 });
